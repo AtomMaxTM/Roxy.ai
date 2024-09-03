2c1086569cc798f5ca420fa921ae5b70bc84628a
AtomMax_ <96258939+AtomMaxTM@users.noreply.github.com>
2024-09-03 20:21:46 +0200

    added REST API for LLM

--- core/servers/nlp_server.py
+++ core/servers/nlp_server.py
@@ -1,15 +1,14 @@
 from typing import Any
 
 from fastapi import FastAPI, APIRouter
-from core.nlp.chat.LLM import Chat, Model
-from core.scripts.config_manager import get_config
-from functools import wraps
-from core.nlp.lang_tools.translate_local import Translator
-from core.nlp.lang_tools.enhance import Enhance
-from core.scripts.tools import Response
-from core.nlp.vectordb.db import MessageDB, VectorDB, Embedder
 from pydantic import BaseModel
 
+from core.nlp.chat.LLM import Chat, Model
+from core.nlp.lang_tools.enhance import Enhance
+from core.nlp.lang_tools.translate_local import Translator
+from core.nlp.vectordb.db import MessageDB, VectorDB, Embedder
+
+# Loading model instances
 
 emb = Embedder('E:/Programming/AI_Models/BERT_Models/bert-base-uncased')
 vdb = VectorDB('src/long_term_memory_db', emb)
@@ -29,20 +28,22 @@ if not response.status:
     raise response.error
 emb.load()
 
-
+# FastAPI
 app = FastAPI()
 
 vdb_router = APIRouter(
     prefix='/vectordb'
 )
+llm_router = APIRouter(
+    prefix='/llm'
+)
+
 
 # Pydantic
 
-class Embed(BaseModel):
+class Text(BaseModel):
     text: str
 
-class CollectionName(BaseModel):
-    name: str
 
 class Add_VDB(BaseModel):
     collection_name: str
@@ -50,35 +51,47 @@ class Add_VDB(BaseModel):
     payloads: list[dict[str, Any]]
     uids: list
 
+
 class Search_VDB(BaseModel):
     collection_name: str
     vectors: list[float]
     top_k: int = 5
 
+
 class Del_by_id_VDB(BaseModel):
     collection_name: str
     ids: list[str]
+
 
 class Add_MDB(BaseModel):
     message: str
     role: str
 
+
 class Search_MDB(BaseModel):
     message: list[float]
     top_k: int = 5
 
+
 class Del_by_id_MDB(BaseModel):
     message_id: str
 
-class TranslatorMessage(BaseModel):
-    message: str
+
+class ModelGenerate(BaseModel):
+    prompt: str
+    max_tokens: int
+    temp: float
+
+
+class ChatRegenerate(BaseModel):
+    tokens: int
 
 
 # Vector Storage
 
 #       Embedder
-@vdb_router.get('/embed')
-async def embed(message: Embed):
+@app.get('/embed')
+async def embed(message: Text):
     message = message.dict()['text']
     data = emb(message).squeeze(0).tolist()[0]
     return {'embedding': data}
@@ -86,8 +99,8 @@ async def embed(message: Embed):
 
 #       Vector DB
 @vdb_router.get('/db/create_collection')
-async def create_collection(name: CollectionName):
-    name = name.dict()['name']
+async def create_collection(name: Text):
+    name = name.dict()['text']
     res = vdb.create_collection(name)
     if res.status == -1:
         return {'status': 0, 'message': res.message}
@@ -137,9 +150,9 @@ async def del_by_id(body: Del_by_id_VDB):
 
 
 @vdb_router.get('/db/delete_collection')
-async def del_delete(body: CollectionName):
+async def del_delete(body: Text):
     try:
-        vdb.delete_collection(body.dict()['collection'])
+        vdb.delete_collection(body.dict()['text'])
     except Exception as e:
         return {'status': 0, 'message': e}
     return {'status': 1}
@@ -185,21 +198,119 @@ async def delete_all():
         return {'status': 0, 'message': e}
     return {'status': 1}
 
+
 # Translator
 
 @app.get('/translate/ru_en')
-async def user_to_llm(text: TranslatorMessage):
-    text = text.dict()['message']
+async def user_to_llm(text: Text):
+    text = text.dict()['text']
     text = translator.ru_to_en(text)
     if not text.status:
         return {'status': 0, 'message': text.error}
     return {'status': 1, 'text': text.data}
 
+
 @app.get('/translate/en_ru')
-async def llm_to_user(text: TranslatorMessage):
-    text = text.dict()['message']
+async def llm_to_user(text: Text):
+    text = text.dict()['text']
     text = translator.en_to_ru(text)
     if not text.status:
         return {'status': 0, 'message': text.error}
     return {'status': 1, 'text': text.data}
 
+
+# Text Enhance
+
+@app.get('/enhance')
+async def enhance(text: Text):
+    text = text.dict()['text']
+    text = e.enhance(text)
+    if not text.status:
+        return {'status': 0, 'message': text.error}
+    return {'status': 1, 'text': text.data}
+
+
+# LLM
+
+#   Model
+@llm_router.get('/raw/generate')
+async def generate(body: ModelGenerate):
+    body = body.dict()
+    generated = md.model(body['prompt'], max_tokens=body['max_tokens'], stop=[f"User:"],
+                         temperature=body['temp'], echo=True)
+    generated = generated["choices"][0]["text"]
+    return {'generated': generated}
+
+
+# @llm_router.get('/raw/')
+
+#   Chat
+
+@llm_router.get('/chat/generate')
+async def chat_generate(body: ModelGenerate):
+    body = body.dict()
+    generate = chat.chat_generate(
+        body['prompt'],
+        body['max_tokens']
+    )
+    return {'generated': generate}
+
+
+@llm_router.get('/chat/regenerate')
+async def chat_regenerate(body: ChatRegenerate):
+    body = body.dict()
+    generate = chat.chat_generate(
+        body['max_tokens']
+    )
+    return {'generated': generate}
+
+
+#   Store
+
+@llm_router.get('/chat/history')
+async def get_history():
+    return chat.store.chat
+
+
+@llm_router.get('/chat/reset')
+async def reset():
+    chat.store.reset_chat()
+    return {'status': 1}
+
+
+@llm_router.get('/chat/system_message')
+async def get_system_message():
+    return {'system_message': chat.store.system_message}
+
+
+@llm_router.get('/chat/set_system_message')
+async def set_system_message(body: Text):
+    body = body.dict()
+    chat.store.change_system_message(body['text'])
+    return {'status': 1}
+
+
+@llm_router.get('/chat/remove_last_message')
+async def remove_last_message():
+    res = chat.store.remove_last_message()
+    if not res.status:
+        return {'status': 0, 'message': res.message}
+    return {'status': 1}
+
+
+@llm_router.get('/chat/change_last_message')
+async def change_last_message(body: Text):
+    body = body.dict()
+    res = chat.store.change_last_message(body['text'])
+    if res.status == -1:
+        return {'status': 0, 'message': res.message}
+    return {'status': 1}
+
+
+@llm_router.get('/chat/change_last_user_message')
+async def change_last_message(body: Text):
+    body = body.dict()
+    res = chat.store.change_last_user_message(body['text'])
+    if res.status == -1:
+        return {'status': 0, 'message': res.message}
+    return {'status': 1}
